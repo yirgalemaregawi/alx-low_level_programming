43fc8beb962a1e00fca8726e1be0a257f3cd81df
yirgalemaregawi <yirgalemaregawi@gmail.com>
2022-10-29 08:43:58 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * return: 0
+ */
+int main(void)
+{
+int i;
+char c;
+for (i = 0; i < 10; i++)
+{
+putchar (i + '0');
+}
+for (c = 'a'; c < 'g'; c++)
+{
+putchar (c);
+}
+putchar ('\n');
+return(0);
+}
